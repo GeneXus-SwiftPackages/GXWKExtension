e98b93ef0797db0c3c5d89fba20371244bbf2a77
Build Er <builder@macmini6.local>
2023-02-16 23:53:10 -0300

    Package version: 1.0.0-beta+20230217024255

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "GXWKExtension",
+	platforms: [.iOS("12.0"), .watchOS("5.0")],
+	products: [
+		.library(
+			name: "GXWKExtension",
+			targets: ["GXWKExtensionWrapper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", branch: "beta"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXUIApplication.git", branch: "beta")
+	],
+	targets: [
+		.target(name: "GXWKExtensionWrapper",
+				dependencies: [
+					"GXWKExtension",
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.watchOS])),
+					.product(name: "GXUIApplication", package: "GXUIApplication", condition: .when(platforms: [.iOS]))
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "GXWKExtension",
+			url: "https://pkgs.genexus.dev/iOS/beta/GXWKExtension-1.0.0-beta+20230217024255.xcframework.zip",
+			checksum: "7e76d0fa5939cf6fbde1137b6c29984f4341373d3880df98d403f94e5150cf28"
+		)
+	]
+)
