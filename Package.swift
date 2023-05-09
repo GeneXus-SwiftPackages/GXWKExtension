35a77ef740b059f3a879997de27b7c0e773940ff
Build Er <builder@macmini6.local>
2023-05-09 14:42:35 -0300

    Package version: 1.0.0-beta+20230509173316

--- Package.swift
+++ Package.swift
@@ -23,8 +23,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXWKExtension",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXWKExtension-1.0.0-beta+20230509131236.xcframework.zip",
-			checksum: "7651fa1a2346755074cd3f335b511d40aed753b0eb8d4a356cc091f3710cd440"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXWKExtension-1.0.0-beta+20230509173316.xcframework.zip",
+			checksum: "270268f7ed5f8fc178cb7a02124c7dc2115fdd9067f73e4f8e03463422085c1b"
 		)
 	]
 )
