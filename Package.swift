ffa799f63cd14b30a340812d29d5295bc0b7bbec
genexusbot <builder@genexus.com>
2023-07-14 16:30:30 -0300

    Package version: 1.0.0-preview.20230714192150

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "GXWKExtension",
+	platforms: [.iOS("12.0"), .watchOS("5.0")],
+	products: [
+		.library(
+			name: "GXWKExtension",
+			targets: ["GXWKExtensionWrapper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.0.0-preview.20230714192150"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXUIApplication.git", exact: "1.0.0-preview.20230714192150")
+	],
+	targets: [
+		.target(name: "GXWKExtensionWrapper",
+				dependencies: [
+					"GXWKExtension",
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.watchOS])),
+					.product(name: "GXUIApplication", package: "GXUIApplication", condition: .when(platforms: [.iOS]))
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "GXWKExtension",
+			url: "https://pkgs.genexus.dev/iOS/preview/GXWKExtension-1.0.0-preview.20230714192150.xcframework.zip",
+			checksum: "dfd3a0dd07bf45e55fb90464d400c9833cf6bd3d67ead76dd0539282f358790b"
+		)
+	]
+)
