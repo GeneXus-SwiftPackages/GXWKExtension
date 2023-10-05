d78a2f7adca8842ce0d91a4484d84c64d6984410
genexusbot <builder@genexus.com>
2023-10-04 23:44:14 -0300

    Package version: 1.2.0-rc.36

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
 			targets: ["GXWKExtensionWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.2.0-rc.29"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXUIApplication.git", exact: "1.2.0-rc.29")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.2.0-rc.36"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXUIApplication.git", exact: "1.2.0-rc.36")
 	],
 	targets: [
 		.target(name: "GXWKExtensionWrapper",
@@ -23,8 +23,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXWKExtension",
-			url: "https://pkgs.genexus.dev/iOS/preview/GXWKExtension-1.2.0-rc.29.xcframework.zip",
-			checksum: "d0bae5352c0b2b3d3862d60bcb89e0569a313268e272cbf7725b8ed89a51ebad"
+			url: "https://pkgs.genexus.dev/iOS/preview/GXWKExtension-1.2.0-rc.36.xcframework.zip",
+			checksum: "85824e11daa0705d9faabf2fc02df74e3287cc1679348bb7e393405b12091540"
 		)
 	]
 )
