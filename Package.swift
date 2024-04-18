109af5b52136ea465bbe18ed19ed0026fbcf309e
Build Er <builder@macmini6.local>
2024-04-18 10:47:21 -0300

    Package version: 2.0.0-beta.7

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
 			targets: ["GXWKExtensionWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "2.0.0-beta.6"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXUIApplication.git", exact: "2.0.0-beta.6")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "2.0.0-beta.7"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXUIApplication.git", exact: "2.0.0-beta.7")
 	],
 	targets: [
 		.target(name: "GXWKExtensionWrapper",
@@ -23,8 +23,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXWKExtension",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXWKExtension-2.0.0-beta.6.xcframework.zip",
-			checksum: "c254c8641c6d571bd288dfbfab1f9fbf44a4abf2188ae7f421faaac9f6ddddc1"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXWKExtension-2.0.0-beta.7.xcframework.zip",
+			checksum: "34a092ded2cabdd95f91deb0ef297253107e3b662ef3106f88a13a84b4c820f7"
 		)
 	]
 )
